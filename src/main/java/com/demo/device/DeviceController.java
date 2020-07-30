64b0b2b2b8e7f6e16b82f1c7d033b9440833cd75
paulaolmedo <paulabeatrizolmedo@gmail.com>
2020-07-30 17:09:59 -0300

    Add POST method to store devices

--- src/main/java/com/demo/device/DeviceController.java
+++ src/main/java/com/demo/device/DeviceController.java
@@ -3,5 +3,12 @@ package com.demo.device;
 @RestController
 @RequestMapping("/device")
 public class DeviceController {
+    //@Autowired
+    //DeviceRepository deviceRepository;
 
+    private final DeviceRepository deviceRepository;
+
+    DeviceController(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
 }
