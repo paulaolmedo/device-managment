b8add2f312563426c3ba93788aefa2f1602d54cb
paulaolmedo <paulabeatrizolmedo@gmail.com>
2020-07-30 17:10:58 -0300

    Add GET method to retrieve all the devices

--- src/main/java/com/demo/device/DeviceController.java
+++ src/main/java/com/demo/device/DeviceController.java
@@ -1,5 +1,11 @@
 package com.demo.device;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/device")
 public class DeviceController {
@@ -10,5 +16,15 @@ public class DeviceController {
 
     DeviceController(DeviceRepository deviceRepository) {
         this.deviceRepository = deviceRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<Object> newDevice(@RequestBody Device device){
+        return ResponseEntity.ok(this.deviceRepository.save(device));
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<Object> getAll(){ 
+        return ResponseEntity.ok(this.deviceRepository.findAll());
     }
 }
