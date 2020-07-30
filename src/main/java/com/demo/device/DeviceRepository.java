012a2d6651b785cb43f37d4d7583d34c0cf77583
paulaolmedo <paulabeatrizolmedo@gmail.com>
2020-07-30 17:16:47 -0300

    Update DeviceRepository Interface

--- src/main/java/com/demo/device/DeviceRepository.java
+++ src/main/java/com/demo/device/DeviceRepository.java
@@ -4,4 +4,5 @@ import java.util.List;
 import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
 
 public interface DeviceRepository extends DatastoreRepository<Device, Long> {
+    List<Device> findByHardwareAddress(String hardwareAddress);
 }
