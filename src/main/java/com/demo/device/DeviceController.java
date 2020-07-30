8470aa51fcac04a745b2a1cbe4accc253d9d25b5
paulaolmedo <paulabeatrizolmedo@gmail.com>
2020-07-30 17:12:18 -0300

    Add GET method to search device by Id

--- src/main/java/com/demo/device/DeviceController.java
+++ src/main/java/com/demo/device/DeviceController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/device")
@@ -22,9 +24,14 @@ public class DeviceController {
     public ResponseEntity<Object> newDevice(@RequestBody Device device){
         return ResponseEntity.ok(this.deviceRepository.save(device));
     }
-    
+
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Object> getAll(){ 
         return ResponseEntity.ok(this.deviceRepository.findAll());
+    }
+
+    @RequestMapping(value = "/{Id}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<Object> getById(@PathVariable (value="Id") final Long Id) {
+        return ResponseEntity.ok(this.deviceRepository.findById(Id));
     }
 }
